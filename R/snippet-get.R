61550138aa55a420b4bef76510275fb08aa92656
naresh kumar <naresh.kumar@mu-sigma.com>
2014-08-07 20:53:07 -0700

    get snippets for anonymous user

--- R/snippet-get.R
+++ R/snippet-get.R
@@ -10,9 +10,9 @@ sni.get.request <- function(id , ctx) {
     params <- signRequest.sni(url, params=character(), consumerKey = cKey, consumerSecret = cSecret, oauthKey= strsplit(ctx$token, "//")[[1]][1] , oauthSecret= strsplit(ctx$token, "//")[[1]][2], httpMethod="GET", signMethod='RSA', handshakeComplete=handshakeComplete)
     params <- lapply(params, encodeURI.sni)
     auth = paste0("OAuth ", paste0(names(params),"=", '"',params, '"', collapse=",") , "")
+    curl <- curlSetOpt(.opts=list(httpheader=c('Authorization'= auth , 'Content-Type' = 'application/json')), verbose=FALSE, curl=getCurlHandle())
   } else {
-    auth = paste0("Basic ", base64Encode("nareshbabral:work4future"), "")
+    curl <- curlSetOpt(.opts=list(httpheader=c('Content-Type' = 'application/json')), verbose=FALSE, curl=getCurlHandle())
   }
-  curl <- curlSetOpt(.opts=list(httpheader=c('Authorization'= auth , 'Content-Type' = 'application/json')), verbose=FALSE, curl=getCurlHandle())
   getURL(url,curl=curl)
 }
